9b7286c42b1da25982bc3965cb1c26abd8dc57f5
ohhGHST <salikluis@outlook.com>
2025-03-28 13:17:42 -0500

    Added info_type function

--- qrcode.py
+++ qrcode.py
@@ -29,40 +29,75 @@ def start():
         print("1. Quick QR Code")
         print("2. Custom QR Code")
 
-        mode = input("Please select between Quick QR Code or Custom QR Code: ").strip()
+        mode_choice = input("Please select between Quick QR Code or Custom QR Code: ").strip()
 
-        if mode == '1':
+        if mode_choice == '1':
             quick_qr()
-        elif mode == '2':
+        elif mode_choice == '2':
             custom_qr()
         else:
             print('Please type a valid number.')
 
+def info_type():
+    while True:
+        print("1. Text")
+        print("2. Website Link")
+        print("3. Contact Information")
+        print("4. Wifi Passwords")
+        print("5. Geolocation")
+        print("6. Image")
+        print("7. EPC (European Payment Council QR Codes)")
+
+        info_type_choice = input("Please select what type of information you are encoding: ").strip()
+
+        if info_type_choice == '1':
+            return 'text'
+        elif info_type_choice == '2':
+            return 'link'
+        elif info_type_choice == '3':
+            return 'contact'
+        elif info_type_choice == '4':
+            return 'wifi'
+        elif info_type_choice == '5':
+            return 'geo'
+        elif info_type_choice == '6':
+            return 'image'
+        elif info_type_choice == '7':
+            return 'epc'
+        else:
+            print('Please type a valid number.')
+
+
+def info_content():
+    pass
+
 def quick_qr():
-    if info_type == text:
+    info_type_choice = info_type()
+    if info_type_choice == 'text':
         pass
-    elif info_type == link:
+    elif info_type_choice == 'link':
         pass
-    elif info_type == contact:
+    elif info_type_choice == 'contact':
         pass
-    elif info_type == wifi:
+    elif info_type_choice == 'wifi':
         pass
-    elif info_type == geo:
+    elif info_type_choice == 'geo':
         pass
     else:
         print('This feature is not yet implemented :(')
         pass
 
 def custom_qr():
-    if info_type == text:
+    info_type_choice = info_type()
+    if info_type_choice == 'text':
         pass
-    elif info_type == link:
+    elif info_type_choice == 'link':
         pass
-    elif info_type == contact:
+    elif info_type_choice == 'contact':
         pass
-    elif info_type == wifi:
+    elif info_type_choice == 'wifi':
         pass
-    elif info_type == geo:
+    elif info_type_choice == 'geo':
         pass
     else:
         print('This feature is not yet implemented :(')
